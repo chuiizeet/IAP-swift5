c43ece8a881df49b9aaa9181ceb680fbced13f6f
chuiizeet <chuy.guerra19@outlook.es>
2019-07-16 16:16:12 -0500

    Init

---
+++ IAP/IAP/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IAP
+//
+//  Created by imac on 7/16/19.
+//  Copyright © 2019 JeguLabs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
