797baa4a9f0968ada8bc6b73b85a7efb313ec00e
chuiizeet <chuy.guerra19@outlook.es>
2019-07-16 17:52:23 -0500

    Setup ui

--- IAP/IAP/ViewController.swift
+++ IAP/IAP/ViewController.swift
@@ -9,10 +9,42 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let purchaseBtn: UIButton = {
+        let btn = UIButton(type: .system)
+        btn.setTitle("Purchase subscription", for: .normal)
+        btn.setTitleColor(.black, for: .normal)
+        btn.backgroundColor = .white
+        btn.layer.borderWidth = 1
+        btn.layer.borderColor = UIColor.black.cgColor
+        btn.layer.cornerRadius = 25
+        
+        btn.addTarget(self, action: #selector(handleBtnPressed), for: .touchUpInside)
+        
+        return btn
+    }()
+    
+    
+    // MARK: - Handlers
+    
+    @objc func handleBtnPressed() {
+        print("Handle btn")
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        setupViewComponents()
+        
+    }
+    
+    func setupViewComponents() {
+        view.backgroundColor = .white
+        
+        view.addSubview(purchaseBtn)
+        purchaseBtn.anchor(top: nil, left: view.leftAnchor, bottom: nil, right: view.rightAnchor, paddingTop: 0, paddingLeft: 20, paddingBottom: 0, paddingRight: 20, width: 0, height: 64)
+        purchaseBtn.centerY(inView: view)
     }
 
 
